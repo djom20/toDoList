25fae2872729f9cff5c1362dddd3c3acbb28a84a
Ing. Jonathan Olier <djom202@gmail.com>
2014-08-01 17:01:18 -0500

    Config Devise

--- db/migrate/20140801154433_create_users.rb
+++ db/migrate/20140801154433_create_users.rb
@@ -3,15 +3,13 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :lastname
-      t.string :email
-      t.string :password
       t.integer :state, :default => 1
-      t.timestamps
+      # t.timestamps
     end
 
-    User.create!(name: 'Jonathan', lastname: 'Olier', email: 'jolier@spes.co', password: 'spes2103')
-    User.create!(name: 'Hector', lastname: 'Villazon', email: 'hector@spes.co', password: 'spes2103')
-    User.create!(name: 'Karen', lastname: 'Sampayo', email: 'karen@spes.co', password: 'spes2103')
-    User.create!(name: 'Paola', lastname: 'Macias', email: 'paola@spes.co', password: 'spes2103')
+    # User.create!(name: 'Jonathan', lastname: 'Olier')
+    # User.create!(name: 'Hector', lastname: 'Villazon')
+    # User.create!(name: 'Karen', lastname: 'Sampayo')
+    # User.create!(name: 'Paola', lastname: 'Macias')
   end
 end
