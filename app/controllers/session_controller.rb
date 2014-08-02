a089841e0836440bdf27dc5e9c92a5ae9dffff03
Ing. Jonathan Olier <djom202@gmail.com>
2014-08-02 10:48:50 -0500

    Config routes for Devise

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,14 @@
 class SessionController < ApplicationController
-  def create
+  
+  def create 
+    user = User.new(params[:user])
+    if user.save
+      render :json=> user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :status=>201
+      return
+    else
+      warden.custom_failure!
+      render :json=> user.errors, :status=>422
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,15 @@
 Rails.application.routes.draw do
+  get 'session/create'
+
+  get 'session/destroy'
+
   devise_for :users
 
-  # get 'users/index'
-  # get 'users/create'
-  # get 'users/show'
-  # get 'users/update'
-  # get 'users/destroy'
-
-  # get 'tickets/index'
-  # get 'tickets/create'
-  # get 'tickets/show'
-  # get 'tickets/update'
-  # get 'tickets/destroy'
-
-  # get 'tasks/index'
-  # get 'tasks/create'
-  # get 'tasks/show'
-  # get 'tasks/update'
-  # get 'tasks/destroy'
-
-  # get 'session/create'
-  # get 'session/destroy'
-
   root 'home#index'
-  
-  scope 'api', defaults: {format: :json} do
-    scope 'v1', defaults: {format: :json} do
-      resources :session, only: [:create, :destroy]
+  scope 'api' do
+    get '/', to: HomeController.action(:index)
+    scope 'v1' do
+      get '/', to: HomeController.action(:index)
       resources :users do 
         resources :tasks do
           resources :tickets
