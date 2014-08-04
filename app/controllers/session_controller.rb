3cfe2290883a7a8e0819f0a028dffe7de276f3e3
Ing. Jonathan Olier <djom202@gmail.com>
2014-08-04 10:30:07 -0500

    Bugs App

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,14 +1,18 @@
 class SessionController < ApplicationController
+
+	before_filter :ensure_params_exist
   
   def create
     build_resource
-    resource = User.find_for_database_authentication(:email => params[:email])
+    resource = User.find_for_database_authentication(:login=>params[:user_login][:login])
     return invalid_login_attempt unless resource
-        
-    if resource.valid_password?(params[:password])
-        resource.ensure_authentication_token!  #make sure the user has a token generated
-        render :json => { :authentication_token => resource.authentication_token, :user_id => resource.id }, :status => :created
-    return
+ 
+    if resource.valid_password?(params[:user_login][:password])
+      sign_in("user", resource)
+      render :json=> {:success=>true, :auth_token=>resource.authentication_token, :login=>resource.login, :email=>resource.email}
+      return
+    end
+    invalid_login_attempt
   end
 
   def destroy
@@ -17,8 +21,14 @@ class SessionController < ApplicationController
     render :json => { :message => ["Session deleted."] },  :success => true, :status => :ok
   end
 
+  protected
+  def ensure_params_exist
+    return unless params[:user_login].blank?
+    render :json=>{:success=>false, :message=>"missing user_login parameter"}, :status=>422
+  end
+ 
   def invalid_login_attempt
     warden.custom_failure!
-    render :json => { :errors => ["Invalid email or password."] },  :success => false, :status => :unauthorized
-	end
+    render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
+  end
 end
