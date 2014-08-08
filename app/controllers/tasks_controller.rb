0d03391f2141888b5aed45d203dd2b02a0064d6b
Ing. Jonathan Olier <djom202@gmail.com>
2014-08-08 08:52:50 -0500

    Fix Cross Domain Rails - AngularJs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,21 @@
 class TasksController < ApplicationController
   respond_to :json
+
+  before_filter :set_headers
   
   def index
     @user = User.find(params[:user_id])
     @user_tasks = @user.tasks
 
-    headers['Access-Control-Allow-Origin'] = '*' 
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    headers['content-type'] = 'application/json'
+    # headers['Access-Control-Allow-Origin'] = '*'
+    #  headers['Access-Control-Allow-Credentials'] = 'true'
+    # headers['Access-Control-Request-Method'] = '*'
+    # headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    # headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    # headers['content-type'] = 'application/json'
 
     if( @user_tasks )
-      render :json => @user_tasks.to_json, :callback => params[:callback]
+      render :json => @user_tasks.to_json, :callback => params[:callback], :content_type => 'application/json'
     else
       render json: { response: nil, error: true, message: 'Not exists tasks' }
     end
@@ -37,5 +40,17 @@ class TasksController < ApplicationController
 
   def destroy
     render json: { response: 'ok', error: false, message: 'Not exists tasks' }
+  end
+
+  def set_headers
+    puts 'ApplicationController.set_headers'
+    if request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
   end
 end
