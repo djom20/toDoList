5fce1f276acc7a658b8234234f5781f2bec494c9
Ing. Jonathan Olier <djom202@gmail.com>
2014-08-11 08:25:38 -0500

    Fix tickets header

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -34,4 +34,16 @@ class TicketsController < ApplicationController
   def destroy
     render json: { response: 'ok', error: false }
   end
+
+  def set_headers
+    puts 'ApplicationController.set_headers'
+    if request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end  
 end
