21fe15c6059054c42c0385f3dd306c66506c8d7e
Ing. Jonathan Olier <djom202@gmail.com>
2014-08-11 12:11:52 -0500

    Fix users request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  respond_to :json
+  before_filter :set_headers
 
   def index
     @users = User.where(:state => 1).order(:name)
@@ -36,4 +38,16 @@ class UsersController < ApplicationController
       render json: { response: nil, error: true, message: 'Not exists user' }
     end
   end
+
+  def set_headers
+    puts 'ApplicationController.set_headers'
+    if request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
+  end  
 end
