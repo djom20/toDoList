1157f3f6405474e960d7dd27da258b228eccb85a
Ing. Jonathan Olier <djom202@gmail.com>
2014-08-13 11:58:50 -0500

    Fix password users 2

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,6 @@
 class SessionController < ApplicationController
+  respond_to :json
+  before_filter :set_headers
   
   def create
     render :json => @task_tickets.to_json, :callback => params[:callback], :content_type => 'application/json'
@@ -6,5 +8,17 @@ class SessionController < ApplicationController
 
   def destroy
   	render :json => @task_tickets.to_json, :callback => params[:callback], :content_type => 'application/json'
+  end
+
+  def set_headers
+    puts 'ApplicationController.set_headers'
+    if request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
   end
 end
